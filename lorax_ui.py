f642a17d38628a756a975ce69dd217a7dbd6bb07
Harish s <harish.s@appstarsapplications.in>
2025-02-08 14:02:39 +0530

    port were added

--- lorax_ui.py
+++ lorax_ui.py
@@ -410,4 +410,4 @@ with gr.Blocks(css=css, head=header) as demo:
 
 
 if __name__ == "__main__":
-    demo.launch(server_name="0.0.0.0", share=True)
+    demo.launch(server_name="0.0.0.0", share=True, server_port=7500)
